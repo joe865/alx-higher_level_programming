6cd7f3e43f159ddef04315e9d52489251f95d98d
joe865 <you@example.com>
2023-01-12 00:20:32 +0000

    python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if not a_dictionary:
+        return None
+    else:
+        name = ''
+        score = 0
+        for key in a_dictionary:
+            if a_dictionary[key] > score:
+                name = key
+                score = a_dictionary[key]
+    return name
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    return {k: a_dictionary[k] * 2 for k in a_dictionary}
+    cpy = a_dictionary.copy()
+    for i in cpy:
+        cpy[i] = cpy[i] * 2
+    return cpy
