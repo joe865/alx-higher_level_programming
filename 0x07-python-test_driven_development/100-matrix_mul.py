826d41de007431551b23550f95216bdaebbb7fef
Your Name <you@example.com>
2023-02-01 13:12:37 -0800

    Python

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,28 +1,65 @@
 #!/usr/bin/python3
+"""
+This is the matrix_mul module.
+This module supplies one function, matrix_mul().
+"""
+
+
 def matrix_mul(m_a, m_b):
-    if not isinstance(m_a, list):
-        raise TypeError('m_a must be a list')
-    if False in [isinstance(listx, list) for listx in m_a]:
-        raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b, list):
-        raise TypeError('m_b must be a list')
-    if False in [isinstance(listx, list) for listx in m_b]:
-        raise TypeError("m_b must be a list of lists")
-    if not len(m_a) or 0 in [len(listx) for listx in m_a]:
+    """
+    Return a new matrix multiplied.
+    Args:
+        m_a (list): list of lists of integers or floats.
+        m_b (list): list of lists of integers or floats.
+    """
+    if type(m_a) is not list:
+        raise TypeError("m_a must be a list")
+    if type(m_b) is not list:
+        raise TypeError("m_b must be a list")
+
+    for row_a in m_a:
+        if type(row_a) is not list:
+            raise TypeError("m_a must be a list of lists")
+    for row_b in m_b:
+        if type(row_b) is not list:
+            raise TypeError("m_b must be a list of lists")
+
+    if len(m_a) > 0:
+        for row_a in m_a:
+            if len(row_a) is 0:
+                raise ValueError("m_a can't be empty")
+    else:
         raise ValueError("m_a can't be empty")
-    if not len(m_b) or 0 in [len(listx) for listx in m_b]:
+    if len(m_b) > 0:
+        for row_b in m_b:
+            if len(row_b) is 0:
+                raise ValueError("m_b can't be empty")
+    else:
         raise ValueError("m_b can't be empty")
-    if any(False in x for x in [[isinstance(ele, (int, float))
-                                 for ele in listx] for listx in m_a]):
-        raise TypeError('m_a should contain only integers or floats')
-    if any(False in x for x in [[isinstance(ele, (int, float))
-                                 for ele in listx] for listx in m_b]):
-        raise TypeError('m_b should contain only integers or floats')
-    if len(set(len(listx) for listx in m_a)) > 1:
-        raise TypeError('each row of m_a must should be of the same size')
-    if len(set(len(listx) for listx in m_b)) > 1:
-        raise TypeError('each row of m_b must should be of the same size')
-    if len(m_a[0]) != len(m_b):
+
+    for row_a in m_a:
+        for i in row_a:
+            if type(i) not in [int, float]:
+                raise TypeError("m_a should contain only integers or floats")
+    for row_b in m_b:
+        for i in row_b:
+            if type(i) not in [int, float]:
+                raise TypeError("m_b should contain only integers or floats")
+
+    row_a_len = 0
+    for row_a in m_a:
+        if len(row_a) is not row_a_len and row_a_len is not 0:
+            raise TypeError("each row of m_a must be of the same size")
+        row_a_len = len(row_a)
+    row_b_len = 0
+    for row_b in m_b:
+        if len(row_b) is not row_b_len and row_b_len is not 0:
+            raise TypeError("each row of m_b must be of the same size")
+        row_b_len = len(row_b)
+
+    if row_a_len is not len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
-    return([[sum(a*b for a, b in
-                 zip(colA, colB)) for colB in zip(*m_b)] for colA in m_a])
+
+    return [[sum(a * b for a, b in zip(row_a, col_b))
+             for col_b in zip(*m_b)] for row_a in m_a]
+
